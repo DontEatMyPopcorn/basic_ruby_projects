25c3aaa840b28ca7b15d6bc905f4bdcabbf5d51a
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-27 18:52:06 -0400

    Complete substrings exercise.

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,17 +1,26 @@
 def substrings(input, dictionary)
   output_hash = Hash.new()
   
-  #break out words
-  #for each
-    #break out subwords
-    #for each
-      #select from dictionary
-        #if not null 
-          output_hash[:test]="test"
-  
+    input_array = input.split(" ")
+    down_input_array = input_array.map{|word| word.downcase}
+    clean_input_array = down_input_array.map{|word| word.gsub(/[^a-zA-Z0-9 ]/, '')}
+    clean_input_array.each do |single_input|
+      new_word = dictionary.select{|word| single_input.include?(word)}
+        new_word.each do |word|
+          if output_hash[word]
+            output_hash[word]+= 1
+          else
+            output_hash[word]= 1
+          end
+          
+        end
+    end
   puts output_hash
 end
 
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+# substrings("below", dictionary)
+# substrings("below low", dictionary)
+# substrings("Below low!", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
