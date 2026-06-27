14d836a5072fa192568ca5ed5084e7edc739faf2
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-06-27 14:40:44 -0400

    Add framework for substrings exercise.

---
+++ sub_strings.rb
@@ -0,0 +1,17 @@
+def substrings(input, dictionary)
+  output_hash = Hash.new()
+  
+  #break out words
+  #for each
+    #break out subwords
+    #for each
+      #select from dictionary
+        #if not null 
+          output_hash[:test]="test"
+  
+  puts output_hash
+end
+
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+substrings("below", dictionary)
